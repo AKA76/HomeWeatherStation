68df2e3e1270953b49bc9f1de05b4adc7ccf5810
Andrei Kucherenko <aka2676@gmail.com>
2018-08-11 06:15:55 +0700

    Initial Commit

---
+++ HomeWeatherStation/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  HomeWeatherStation
+//
+//  Created by Andrei Kucherenko on 11/08/2018.
+//  Copyright © 2018 AK. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
